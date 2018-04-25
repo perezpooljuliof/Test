18bcc7e34b523d77252269fe67713ec1b5553f7c
jperez <julio.perez@cardinal-bi.com.mx>
2018-04-25 14:48:00 -0500

    save

---
+++ src/main/java/mx/com/example/test/config/security/RedisSessionConfig.java
@@ -0,0 +1,109 @@
+package mx.com.example.test.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisSessionConfig {
+
+    public RedisSessionConfig() {
+        System.out.println("RedisSessionConfig>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    }
+
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory();
+        connectionFactory.setPassword(null);
+        return connectionFactory;
+    }
+
+
+    /*
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory(JedisPoolConfig config) {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName("localhost");
+        factory.setPort(6379);
+        factory.setPoolConfig(config);
+        return factory;
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        return template;
+    }
+    */
+
+    /*
+    @Bean
+    public RedisConnectionFactory lettuceConnectionFactory() {
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+                .master("mymaster")
+                .sentinel("127.0.0.1", 26379)
+                .sentinel("127.0.0.1", 26380);
+        return new LettuceConnectionFactory(sentinelConfig);
+    }
+    */
+
+    /*
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        return new JedisConnectionFactory();
+    }
+    */
+
+    /*
+    @Bean
+    RedisTemplate<Object, Object> redisTemplate() {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<Object, Object>();
+        redisTemplate.setConnectionFactory(connectionFactory());
+        return redisTemplate;
+    }
+
+
+    @Bean
+    public RedisTemplate redisTemplate() {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory());
+        return redisTemplate;
+    }
+
+    @Bean
+    public StringRedisTemplate strRedisTemplate() {
+        StringRedisTemplate redisTemplate = new StringRedisTemplate();
+        redisTemplate.setConnectionFactory(connectionFactory());
+        return redisTemplate;
+    }
+    */
+
+
+    /*
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName("localhost");
+        factory.setPort(6379);
+        factory.setUsePool(true);
+        return factory;
+    }
+
+    @Bean
+    RedisTemplate< String, Object > redisTemplate() {
+        final RedisTemplate< String, Object > template =  new RedisTemplate< String, Object >();
+        template.setConnectionFactory( connectionFactory() );
+        template.setKeySerializer( new StringRedisSerializer() );
+        template.setHashValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
+        template.setValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
+        return template;
+    }
+    */
+}
